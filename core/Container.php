31858c550bbcf51ba649da251dde5fe3ad53c9c3
syd <syd3050@163.com>
2018-10-30 15:48:53 +0800

    Update Container

--- core/Container.php
+++ core/Container.php
@@ -10,6 +10,8 @@ namespace core;
 
 
 use core\exception\ConfigException;
+use core\exception\ServerException;
+use http\Exception\RuntimeException;
 
 /**
  *
@@ -36,96 +38,47 @@ class Container
     public $building = [];
 
     /**
-     * 注册一个绑定到容器
+     * 绑定到容器
+     * @param $abstract
+     * @param $concrete
+     * @param bool $shared
      */
-    public function bind($abstract='Dog', $concrete = 'App\Dog', $shared = false)
+    public function bind($abstract, $concrete, $shared = false)
     {
-        if(is_null($concrete)){
-            $concrete = $abstract;
+        if(empty($concrete)){
+            throw new \RuntimeException(__CLASS__."::".__FUNCTION__.".Parameter concrete is needed!");
         }
-
         if(!$concrete instanceOf \Closure){
-            $concrete = $this->getClosure($abstract, $concrete);
-            /**
-            $concrete = function($c) use ('Dog', 'App\Dog') {
-            return $c->make('App\Dog');
-            }
-             */
+            $concrete = function(Container $c) use($concrete){
+                return $c->make($concrete);
+            };
         }
-        /**
-        $this->building['Dog'] = [
-        'concrete' => function($c) use ('Dog', 'App\Dog') {
-        return $c->make('App\Dog');
-        },
-        'shared'   => false
-        ];
-         */
         $this->building[$abstract] =  compact("concrete", "shared");
     }
 
-    //注册一个共享的绑定 单例
     public function singleton($abstract, $concrete, $shared = true){
         $this->bind($abstract, $concrete, $shared);
     }
 
     /**
-     * 默认生成实例的回调闭包
-     *
-     * @param $abstract
-     * @param $concrete
-     * @return \Closure
-     */
-    public function getClosure($abstract='Dog', $concrete = 'App\Dog')
-    {
-        /**
-        return function($c) use ('Dog', 'App\Dog') {
-        return $c->make('App\Dog');
-        }
-
-         */
-        return function($c) use($abstract, $concrete){
-            $method = ($abstract == $concrete)? 'build' : 'make';
-
-            return $c->$method($concrete);
-        };
-    }
-
-    /**
      * 生成实例
+     * @param $abstract
+     * @return mixed|object
+     * @throws ServerException
      */
-    public function make($abstract='App\Dog')
+    public function make($abstract)
     {
-        /**
-         * $concrete = 'App\Dog';
-         */
         $concrete = $this->getConcrete($abstract);
-
-        if($this->isBuildable($concrete, $abstract)){
-            //$object = new Dog(...);
-            $object = $this->build($concrete);
-        }else{
-            $object = $this->make($concrete);
-        }
-
-        return $object;
+        return $this->build($concrete);
     }
 
     /**
-     * 获取绑定的回调函数
-     */
-    public function getConcrete($abstract='App\Dog')
-    {
-        if(! isset($this->building[$abstract])){
-            return $abstract;
-        }
-
-        return $this->building[$abstract]['concrete'];
-    }
-
-    /**
-     * 判断 是否 可以创建服务实体
+     * 是否可以创建服务实体
      * 1.具体类和抽象类一致，可以build
      * 2.具体类是一个闭包，可以build
+     * @param $concrete
+     * @param $abstract
+     * @return bool
      */
     public function isBuildable($concrete, $abstract)
     {
@@ -133,35 +86,48 @@ class Container
     }
 
     /**
+     * 获取绑定的回调函数
+     * 1.abstract已经绑定过，直接返回回调函数
+     * 2.abstract没有绑定过，返回它本身
+     * @param $abstract
+     * @return mixed
+     */
+    public function getConcrete($abstract)
+    {
+        if(! isset($this->building[$abstract])){
+            return $abstract;
+        }
+        return $this->building[$abstract]['concrete'];
+    }
+
+    /**
      * 根据实例具体名称实例具体对象
+     * @param $concrete
+     * @return mixed|object
+     * @throws ServerException
      */
     public function build($concrete)
     {
-        /**
-         * 闭包则直接返回
-         */
         if($concrete instanceof \Closure){
             return $concrete($this);
         }
-
-        //创建反射对象
-        $reflector = new \ReflectionClass($concrete);
-
-        if( ! $reflector->isInstantiable()){
+        try{
+            $reflector = new \ReflectionClass($concrete);
+            if( ! $reflector->isInstantiable()){
+                //抛出异常
+                throw new ServerException(__CLASS__."::".__FUNCTION__.'无法实例化'.$concrete);
+            }
+            $constructor = $reflector->getConstructor();
+            if(is_null($constructor)){
+                return new $concrete;
+            }
+            $dependencies = $constructor->getParameters();
+            $instance = $this->getDependencies($dependencies);
+            return $reflector->newInstanceArgs($instance);
+        }catch (\Exception $e) {
             //抛出异常
-            throw new \Exception('无法实例化');
+            throw new ServerException(__CLASS__."::".__FUNCTION__.'message:'.$e->getMessage().',code:'.$e->getCode());
         }
-
-        $constructor = $reflector->getConstructor();
-        if(is_null($constructor)){
-            return new $concrete;
-        }
-
-        $dependencies = $constructor->getParameters();
-        $instance = $this->getDependencies($dependencies);
-
-        return $reflector->newInstanceArgs($instance);
-
     }
 
     //通过反射解决参数依赖
@@ -183,7 +149,7 @@ class Container
         if($parameter->isDefaultValueAvailable()){
             return $parameter->getDefaultValue();
         }
-        throw new ConfigException(__CLASS__."::".__FUNCTION__.".");
+        throw new ConfigException(__CLASS__."::".__FUNCTION__.".参数{$parameter->getName()}没有默认值");
 
     }
 
